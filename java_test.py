0a2943d167aedd6d45e65122542a81645b39d905
Kevin Klein <kevin@tabolo.de>
2017-12-16 01:20:41 +0100

    extractors for java and javascript

--- java_test.py
+++ java_test.py
@@ -1,45 +1,136 @@
+import sys
+sys.path.append('/Users/kevinklein/javalang')
+
 from worker.models import *
 
 e = JavaExtractor()
-tree = e.extract("""
+fragments = e.extract("""
+package org.softlang.company.features;
 
-package org.softlang.company.model;
+import org.softlang.company.xjc.*;
 
-import java.io.Serializable;
-import java.util.LinkedList;
-import java.util.List;
+public class Total {
 
-public class Company implements Serializable {
+    public static double total(Company c) {
+        double total = 0;
+        for (Department d : c.getDepartment())
+            total += total(d);
+        return total;
+    }
 
-	private static final long serialVersionUID = -200889592677165250L;
+    public static double total(Department d) {
+        double total = total(d.getManager());
+        for (Subunit s : d.getSubunit())
+            total += total(s);
+        return total;
+    }
 
-	private String name;
-	private List<Department> depts = new LinkedList<Department>();
+    public static double total(Employee e) {
+        return e.getSalary();
+    }
 
-	public String getName() {
-		return name;
-	}
+    //
+    // We cannot use virtual methods.
+    // That is, we do not allow ourselves modifying schema-derived classes.
+    //
+    public static double total(Subunit s) {
+        if (s instanceof Department)
+            return total(((Department)s));
+        else if (s instanceof Employee)
+            return total(((Employee)s));
+        else throw new IllegalArgumentException();
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+}
 
-	public List<Department> getDepts() {
-		return depts;
-	}
+""")
 
-	public Double total() {
-		double total = 0;
-		for (Department d : getDepts())
-			total += d.total();
-		return total;
-	}
+import json
 
-	public void cut() {
-		for (Department d : getDepts())
-			d.cut();
-	}
+print(json.dumps(fragments, indent=4))
+
+e2 = JavaScriptExtractor()
+fragments = e2.extract("""
+var x = function(y) {
+	return 5;
+}
+
+function extractFloat(o){return parseFloat(o.textContent);}
+function setFloat(o,v){o.textContent = v;}
+function allTrue(arr){
+  var ret = true;
+  jQuery.each(arr,function(i,a){ret = ret && a;});
+  return ret;
+}
+function cut(){
+  $(".salary > .int").contents().each(function(){
+    var old = extractFloat(this);
+    setFloat(this, old/2);
+  });
+}
+function total(){
+  var t = 0;
+  $(".salary > .int").contents().each(function(){
+    t = t + extractFloat(this);
+  });
+  alert(t);
+}
+// todo: works now but I think it is doing extra calculation by finding the departments more than once.
+function depth(jqo){
+  var depts = $(jqo).find(".department")
+  var depths = jQuery.map(depts,function(a){return depth($(a));});
+  var add = 0;
+  if (jqo.attr("class") == "department"){add = 1;}
+  if (depths.length == 0) {return add;} else {return  (Math.max.apply(null, depths) + add);}
+}
+function check_precedence(m,jqo){
+  if (jqo.attr("class") ==  "department") {
+    var manager_sal = parseFloat(jqo.children().slice(1,2).children().slice(2,3).first().text())
+    return allTrue(jqo.children().slice(2).map(function(){return check_precedence(manager_sal, $(this))}));
+  }
+  else if (jqo.attr("class") ==  "employee") {
+    var employee_sal = parseFloat(jqo.children().slice(2,3).first().text());
+    return (m > employee_sal);
+  }
+  else if (jqo.attr("class") ==  "manager") {
+    var manager_sal = parseFloat(jqo.children().slice(2,3).first().text());
+    return (m > manager_sal);
+  }
+  else {
+    return allTrue(jqo.children().map(function(){return check_precedence(m, $(this))}));
+  }
 }
 """)
 
-print(e.to_string(tree))
+print(json.dumps(fragments, indent=4))
+
+
+e3 = PythonExtractor()
+fragments = e3.extract("""
+from .worker import *
+
+import javalang
+import javalang.tree
+
+import esprima
+
+import ast
+
+class JavaExtractor(object):
+
+	def extract(self, source):
+	    self.source = source
+	    tree = javalang.parse.parse(source)
+
+	    return {
+	        'imports': self._extract_imports(tree),
+	        'package': self._extract_package(tree),
+	        'fragments': self._extract_fragments(tree)
+	    }
+
+	def _extract_annotations(self, tree):
+	    return [annotation.name for annotation in tree.annotations]
+
+""")
+
+# print(fragments)
--- worker/models.py
+++ worker/models.py
@@ -1,288 +1,150 @@
 from .worker import *
 
 import javalang
+import javalang.tree
 
-class PrettyPrinter(object):
+import esprima
 
-    def emit_CompilationUnit(self, node):
-        fragments = []
-        for t in node.types:
-            fragments.append(self.render_node(t))
-
-        return fragments
-
-    def emit_Import(self, node):
-        print(node.__dict__)
-
-    def emit_Documented(self, node):
-        pass
-
-    def emit_Declaration(self, node):
-        pass
-
-    def emit_TypeDeclaration(self, node):
-        pass
-
-    def emit_PackageDeclaration(self, node):
-        print(node.__dict__)
-
-    def emit_ClassDeclaration(self, node):
-        body = [self.render_node(n) for n in node.body]
-        return {
-            'classifier': 'class',
-            'body': body
-        }
-
-    def emit_EnumDeclaration(self, node):
-        pass
-
-    def emit_InterfaceDeclaration(self, node):
-        pass
-
-    def emit_AnnotationDeclaration(self, node):
-        pass
-
-    def emit_Type(self, node):
-        pass
-
-    def emit_BasicType(self, node):
-        dimensions = self.render_dimensions(node)
-        return "%s%s" % (node.name, dimensions)
-
-    def emit_ReferenceType(self, node):
-        dimensions = self.render_dimensions(node)
-        return "%s%s" % (node.name, dimensions)
-
-    def render_dimensions(self, node):
-        return '[]' * len(node.dimensions)
-
-    def emit_TypeArgument(self, node):
-        pass
-
-    def emit_TypeParameter(self, node):
-        pass
-
-    def emit_Annotation(self, node):
-        pass
-
-    def emit_ElementValuePair(self, node):
-        pass
-
-    def emit_ElementArrayValue(self, node):
-        pass
-
-    def emit_Member(self, node):
-        pass
-
-    def emit_MethodDeclaration(self, node):
-        modifiers = ' '.join(node.modifiers).strip()
-
-        name = node.name
-        return_type = self.render_node(node.return_type)
-
-        params = ', '.join([self.render_node(n) for n in node.parameters])
-
-        print(node.body)
-        body = '\n'.join([self.render_node(n) for n in node.body])
-
-        return {
-            'classifier': 'method',
-            'name': name,
-            'code': ('%s %s %s(%s) { %s }' % (modifiers, return_type, name, params, body)).strip()
-        }
-
-    def emit_NoneType(self, node):
-        return 'void'
-
-    def emit_FieldDeclaration(self, node):
-        pass
-
-    def emit_ConstructorDeclaration(self, node):
-        pass
-
-    def emit_ConstantDeclaration(self, node):
-        pass
-
-    def emit_ArrayInitializer(self, node):
-        pass
-
-    def emit_VariableDeclaration(self, node):
-        pass
-
-    def emit_LocalVariableDeclaration(self, node):
-        # print(node.__dict__)
-        # print(node.declarators)
-        return "%s %s" % (self.render_node(node.type), ', '.join([self.render_node(n) for n in node.declarators]))
-
-    def emit_VariableDeclarator(self, node):
-        print(node.__dict__)
-        return "%s = %s" % (node.name)
-
-    def emit_FormalParameter(self, node):
-        return "%s %s" % (self.render_node(node.type), node.name)
-
-    def emit_InferredFormalParameter(self, node):
-        pass
-
-    def emit_Statement(self, node):
-        pass
-
-    def emit_IfStatement(self, node):
-        pass
-
-    def emit_WhileStatement(self, node):
-        pass
-
-    def emit_DoStatement(self, node):
-        pass
-
-    def emit_ForStatement(self, node):
-        pass
-
-    def emit_AssertStatement(self, node):
-        pass
-
-    def emit_BreakStatement(self, node):
-        pass
-
-    def emit_ContinueStatement(self, node):
-        pass
-
-    def emit_ReturnStatement(self, node):
-        return 'return %s;' % self.render_node(node.expression)
-
-    def emit_ThrowStatement(self, node):
-        pass
-
-    def emit_SynchronizedStatement(self, node):
-        pass
-
-    def emit_TryStatement(self, node):
-        pass
-
-    def emit_SwitchStatement(self, node):
-        pass
-
-    def emit_BlockStatement(self, node):
-        pass
-
-    def emit_StatementExpression(self, node):
-        return "%s;" % self.render_node(node.expression)
-
-    def emit_TryResource(self, node):
-        pass
-
-    def emit_CatchClause(self, node):
-        pass
-
-    def emit_CatchClauseParameter(self, node):
-        pass
-
-    def emit_SwitchStatementCase(self, node):
-        pass
-
-    def emit_ForControl(self, node):
-        pass
-
-    def emit_EnhancedForControl(self, node):
-        pass
-
-    def emit_Expression(self, node):
-        pass
-
-    def emit_Assignment(self, node):
-        pass
-
-    def emit_TernaryExpression(self, node):
-        pass
-
-    def emit_BinaryOperation(self, node):
-        pass
-
-    def emit_Cast(self, node):
-        pass
-
-    def emit_MethodReference(self, node):
-        pass
-
-    def emit_LambdaExpression(self, node):
-        pass
-
-    def emit_Primary(self, node):
-        pass
-
-    def emit_Literal(self, node):
-        return node.value
-
-    def emit_This(self, node):
-        pass
-
-    def emit_MemberReference(self, node):
-        return node.member
-
-    def emit_Invocation(self, node):
-        pass
-
-    def emit_ExplicitConstructorInvocation(self, node):
-        pass
-
-    def emit_SuperConstructorInvocation(self, node):
-        pass
-
-    def emit_MethodInvocation(self, node):
-        args = ' '.join([self.render_node(n) for n in node.arguments]).strip()
-        return '%s.%s(%s)' % (node.qualifier, node.member, args)
-
-    def emit_SuperMethodInvocation(self, node):
-        pass
-
-    def emit_SuperMemberReference(self, node):
-        pass
-
-    def emit_ArraySelector(self, node):
-        pass
-
-    def emit_ClassReference(self, node):
-        pass
-
-    def emit_VoidClassReference(self, node):
-        pass
-
-    def emit_Creator(self, node):
-        pass
-
-    def emit_ArrayCreator(self, node):
-        pass
-
-    def emit_ClassCreator(self, node):
-        pass
-
-    def emit_InnerClassCreator(self, node):
-        pass
-
-    def emit_EnumBody(self, node):
-        pass
-
-    def emit_EnumConstantDeclaration(self, node):
-        pass
-
-    def emit_AnnotationMethod(self, node):
-        pass
-
-    def render_node(self, node):
-        # print(node.__class__.__name__)
-        emit = getattr(self, 'emit_%s' % node.__class__.__name__, self.default_emit)
-        return emit(node)
-
-    def default_emit(self, node):
-        """Fallback function for emitting unknown nodes."""
-        raise NotImplementedError("Node '%s' unknown" % node.__class__.__name__)
+import ast, asttokens
 
 class JavaExtractor(object):
 
     def extract(self, source):
-        return javalang.parse.parse(source)
+        self.source = source
+        tree = javalang.parse.parse(source)
 
-    def to_string(self, node):
-        printer = PrettyPrinter()
-        return printer.render_node(node)
+        return {
+            'imports': self._extract_imports(tree),
+            'package': self._extract_package(tree),
+            'fragments': self._extract_fragments(tree)
+        }
+
+    def _extract_imports(self, tree):
+        return [i.path for i in tree.imports]
+
+    def _extract_package(self, tree):
+        return tree.package.name
+
+    def _extract_fragments(self, tree):
+        fragments = []
+        for t in tree.types:
+            fragments.append(self._extract_fragment(t))
+
+        return fragments
+
+    def _extract_fragment(self, node):
+        end = node.position.end
+        if end is None:
+            end = (len(self.source.split('\n')), 0)
+
+        base = {
+            'start': node.position.start[0],
+            'end': end[0]
+        }
+        if isinstance(node, javalang.tree.ClassDeclaration):
+            return {
+                **base,
+                'classifier': 'class',
+                'name': node.name,
+                'annotations': self._extract_annotations(node),
+                'fragments': [self._extract_fragment(i) for i in node.body]
+            }
+        elif isinstance(node, javalang.tree.FieldDeclaration):
+            names = [n.name for n in node.declarators]
+            return {
+                **base,
+                'classifier': 'field',
+                'names': names,
+                'type': node.type.name
+            }
+        elif isinstance(node, javalang.tree.MethodDeclaration):
+            params = [{
+                **base,
+                'name': param.name,
+                'type': param.type.name,
+                'annotations': param.annotations,
+                'modifiers': list(param.modifiers)
+            } for param in node.parameters]
+            return {
+                **base,
+                'name': node.name,
+                'classifier': 'method',
+                'params': params,
+                'annotations': [],
+                'modifiers': list(node.modifiers)
+            }
+        elif isinstance(node, javalang.tree.ConstructorDeclaration):
+            params = [{
+                **base,
+                'name': param.name,
+                'type': param.type.name,
+                'annotations': param.annotations,
+                'modifiers': list(param.modifiers)
+            } for param in node.parameters]
+
+            return {
+                **base,
+                'classifier': 'constructor',
+                'params': params,
+                'annotations': [],
+                'modifiers': list(node.modifiers)
+            }
+        else:
+            print(node)
+            raise
+
+    def _extract_annotations(self, tree):
+        return [annotation.name for annotation in tree.annotations]
+
+class JavaScriptExtractor(object):
+
+    def __init__(self):
+        pass
+
+    def extract(self, source):
+        tree = esprima.parse(source, loc=True)
+
+        fragments = [self._extract_fragment(node) for node in tree.body]
+
+        return fragments
+
+    def _extract_fragment(self, node):
+        if node.type == 'FunctionDeclaration':
+            params = [param.name for param in node.params]
+            return {
+                'classifier': 'function',
+                'start': node.loc.start.line,
+                'end': node.loc.end.line,
+                'name': node.id.name,
+                'params': params
+            }
+        elif node.type == 'VariableDeclaration':
+            names = [decl.id.name for decl in node.declarations]
+            return {
+                'classifier': 'variable',
+                'start': node.loc.start.line,
+                'end': node.loc.end.line,
+                'names': names
+            }
+        else:
+            print(node.toDict())
+            raise
+
+class PythonExtractor(object):
+
+    def extract(self, source):
+        tree = asttokens.ASTTokens(source, parse=True)
+        # print(tree.body)
+        # print(dir(tree.body))
+
+        print(dir(tree.tree))
+        for node in tree.tree.body:
+            self._extract_fragment(node)
+
+    def _extract_fragment(self, node):
+        if isinstance(node, ast.ClassDef):
+            print(dir(node))
+            return {
+                'classifier': 'class',
+                'name': node.name
+            }
